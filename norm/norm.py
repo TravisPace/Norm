62c44f43052a647a35421f87130c896b18c4f180
TravisPace <40004711+TravisPace@users.noreply.github.com>
2018-06-21 14:31:13 -0500

    Update norm.py

--- norm/norm.py
+++ norm/norm.py
@@ -12,6 +12,12 @@ def norm2(vector):
   return result
 
 def norm3(vector):
+  '''
+  This function calculates the infinity norm. Result is the norm of the vector. For every element in the vector, the algorithim takes the
+  absolute value of the element. If the element is bigger, then the algorithim takes the number as the new result. If the element is
+  smaller than the previous element, then the algorithim keeps the previous result. The algorithim goes until there are no more
+  elements in the list.
+  '''
   result = 0
   for i in range(len(vector)):
     if result <= abs(vector[i]):
