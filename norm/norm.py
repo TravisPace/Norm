a3ad1461a8c12905025c54a845b305b42ac40a93
TravisPace <40004711+TravisPace@users.noreply.github.com>
2018-06-29 14:20:34 -0500

    Update norm.py

--- norm/norm.py
+++ norm/norm.py
@@ -5,10 +5,17 @@ def norm1(vector):
   return result
 
 def norm2(vector):
+  '''
+  This function calculates the two norm. The result is the norm of the vector. This takes the absolute value of every element and squares
+  them. This adds the result of the after each element is squared. Then, the function takes the square root of the result to return
+  the two norm.
+  '''
   result = 0
   for i in range(len(vector)):
     result = result +abs(vector[i])**2
+    #Takes each element and squares the element and adds them together.
   result = result**.5
+  #This takes the result and raises it to the half power, which is equivalent to a square root.
   return result
 
 def norm3(vector):
